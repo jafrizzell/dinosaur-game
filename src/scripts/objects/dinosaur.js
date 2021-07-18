d497e46cb2c5c47e6ffa109ee1528b074e98976c
jafrizzell <69974492+jafrizzell@users.noreply.github.com>
2021-07-17 19:06:06 -0500

    Update dinosaur.js

--- src/scripts/objects/dinosaur.js
+++ src/scripts/objects/dinosaur.js
@@ -32,16 +32,11 @@
 		if (distanceRemaining > 0) {
 			var maxPoint = JUMP_DISTANCE / 2;
 
-			if (distanceRemaining >= maxPoint) {
-				distanceRemaining -= JUMP_DISTANCE
-			}
-
 			// get a number between 0 and 1 (-x^2)
-			// var arcPos = Math.abs(Math.pow(distanceRemaining / maxPoint, 2) * -1);
-
-			// linear
-			var arcPos = Math.abs(distanceRemaining / maxPoint);
-
+			if (distanceRemaining > maxPoint) {
+				var arcPos = -1 * Math.pow(distanceRemaining/maxPoint - 1, 2) + 1;
+			}
+			else {var arcPos = Math.pow((maxPoint-distanceRemaining)/maxPoint - 1 , 2);}
 			return JUMP_HEIGHT * arcPos;
 		}
 		return 0;
@@ -64,63 +59,111 @@
 		// context.fillStyle = backgroundColour;
 		// context.fillRect(x + 7, y - 14, 22, 18);
 
-		// Dino!
-		context.fillStyle = this.colour;
+		// fix backgroudn horizon line
+		context.fillStyle = "#000000";
+		context.fillRect(0, this.height-10, 500, 1);
+
+		context.fillStyle = "#514f59";
 		
-		// tail
-		context.fillRect(x, y - 36, 2, 16);
-		context.fillRect(x + 2, y - 32, 2, 16);
-		context.fillRect(x + 4, y - 30, 2, 16);
-		context.fillRect(x + 6, y - 28, 2, 16);
-		context.fillRect(x + 8, y - 28, 2, 18);
-		context.fillRect(x + 10, y - 30, 2, 22);
-		context.fillRect(x + 12, y - 32, 4, 26);
-		context.fillRect(x + 16, y - 34, 4, 26);
-		context.fillRect(x + 20, y - 36, 4, 30);
-		context.fillRect(x + 24, y - 38, 2, 30);
-		context.fillRect(x + 26, y - 38, 2, 28);
-		context.fillRect(x + 28, y - 52, 2, 40);
+		// front arm
+		var xscale = 3
+		var yscale = 3 
+
+		context.fillRect(x + 0, y - 8*yscale, 1*xscale, 3*yscale);
+		context.fillRect(x + 1*xscale, y - 9*yscale, 1*xscale, 5*yscale);
+		context.fillRect(x + 2*xscale, y - 9*yscale, 1*xscale, 6*yscale);
+		context.fillRect(x + 3*xscale, y - 8*yscale, 1*xscale, 1*yscale);
+		context.fillRect(x + 3*xscale, y - 6*yscale, 1*xscale, 1*yscale);
+		
+		// back arm
+		context.fillRect(x + 4*xscale, y - 9*yscale, 3*xscale, 1*yscale);
+		context.fillRect(x + 6*xscale, y - 8*yscale, 2*xscale, 1*yscale);
+
+		// chest / stomach
+
+		context.fillStyle = "#141021"
+		context.fillRect(x, y - 5*yscale, 1*xscale, 1*yscale);
+		context.fillRect(x + 1*xscale, y - 4*yscale, 1*xscale, 1*yscale);
+		context.fillRect(x + 2*xscale, y - 3*yscale, 3*xscale, 1*yscale);
+		context.fillRect(x + 5*xscale, y - 4*yscale, 1*xscale, 1*yscale);
+		context.fillRect(x + 6*xscale, y - 7*yscale, 2*xscale, 4*yscale);
+		context.fillRect(x + 8*xscale, y - 6*yscale, 1*xscale, 2*yscale);
+		context.fillRect(x + 4*xscale, y - 8*yscale, 2*xscale, 3*yscale);
+		context.fillRect(x + 3*xscale, y - 9*yscale, 1*xscale, 1*yscale);
+		context.fillRect(x + 3*xscale, y - 7*yscale, 1*xscale, 1*yscale);
+
+		// Hands
+		context.fillStyle = "#cfc7e8";
+		context.fillRect(x + 3*xscale, y - 4*yscale, 2*xscale, 1*yscale);
+		context.fillRect(x + 3*xscale, y - 5*yscale, 3*xscale, 1*yscale);
+		context.fillRect(x + 5*xscale, y - 7*yscale, 1*xscale, 1*yscale);
+		context.fillRect(x + 8*xscale, y - 7*yscale, 1*xscale, 1*yscale);
+
 
 		if (this.wideEyed) {
-			context.fillRect(x + 30, y - 54, 6, 2);
-			context.fillRect(x + 32, y - 50, 2, 2);
-			context.fillRect(x + 30, y - 46, 2, 32);
-			context.fillRect(x + 32, y - 46, 2, 30);
-			context.fillRect(x + 34, y - 46, 2, 28);
-			// context.fillRect(x + 32, y - 54, 2, 4);
-			// context.fillRect(x + 32, y - 48, 2, 32);
-			// context.fillRect(x + 34, y - 54, 2, 36);
-		} else {
-			context.fillRect(x + 30, y - 54, 2, 40);
-			context.fillRect(x + 32, y - 54, 2, 4);
-			context.fillRect(x + 32, y - 48, 2, 32);
-			context.fillRect(x + 34, y - 54, 2, 36);
-		}
+			// head & hair
+			context.fillStyle = "#cca951";
+			context.fillRect(x + 2*xscale, y - 16*yscale, 6*xscale, 7*yscale);
+			context.fillRect(x + 1*xscale, y - 13*yscale, 1*xscale, 2*yscale);
+			context.fillRect(x + 7*xscale, y - 16*yscale, 1*xscale, 6*yscale);
+			context.fillRect(x + 8*xscale, y - 13*yscale, 1*xscale, 4*yscale);
+			context.fillRect(x + 9*xscale, y - 13*yscale, 1*xscale, 3*yscale);
+			context.fillRect(x + 8*xscale, y - 14*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 6.5*xscale, y - 15.5*yscale, 1*xscale, 1*yscale);
+			context.fillStyle = "#231d10";
+			context.fillRect(x, y - 14*yscale, 1*xscale, 4*yscale);
+			context.fillRect(x + 1*xscale, y - 15*yscale, 1*xscale, 2*yscale);
+			context.fillRect(x + 2*xscale, y - 16*yscale, 1*xscale, 5*yscale);
+			context.fillRect(x + 2*xscale, y - 16*yscale, 1*xscale, 5*yscale);
+			context.fillRect(x + 3*xscale, y - 16*yscale, 2*xscale, 1*yscale);
+			context.fillRect(x + 3*xscale, y - 14*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 3*xscale, y - 12*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 1*xscale, y - 11*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 6*xscale, y - 11*yscale, 4*xscale, 1*yscale);
+			context.fillRect(x + 7*xscale, y - 12*yscale, 1*xscale, 1*yscale);
+			context.fillStyle = "#000000"
+			context.fillRect(x + 6*xscale, y - 14*yscale, 1*xscale, 2*yscale);
 
-		context.fillRect(x + 36, y - 54, 2, 34);
-		context.fillRect(x + 38, y - 54, 2, 20);
-		context.fillRect(x + 40, y - 54, 12, 16);
-		context.fillRect(x + 52, y - 52, 2, 14);
+		} else {
+			// head & hair
+			context.fillStyle = "#cca951";
+			context.fillRect(x + 2*xscale, y - 16*yscale, 6*xscale, 7*yscale);
+			context.fillRect(x + 1*xscale, y - 13*yscale, 1*xscale, 2*yscale);
+			context.fillRect(x + 7*xscale, y - 16*yscale, 1*xscale, 6*yscale);
+			context.fillRect(x + 8*xscale, y - 13*yscale, 1*xscale, 4*yscale);
+			context.fillRect(x + 9*xscale, y - 13*yscale, 1*xscale, 3*yscale);
+			context.fillRect(x + 8*xscale, y - 14*yscale, 1*xscale, 1*yscale);
+			context.fillStyle = "#231d10";
+			context.fillRect(x, y - 14*yscale, 1*xscale, 4*yscale);
+			context.fillRect(x + 1*xscale, y - 15*yscale, 1*xscale, 2*yscale);
+			context.fillRect(x + 2*xscale, y - 16*yscale, 1*xscale, 5*yscale);
+			context.fillRect(x + 2*xscale, y - 16*yscale, 1*xscale, 5*yscale);
+			context.fillRect(x + 3*xscale, y - 16*yscale, 2*xscale, 1*yscale);
+			context.fillRect(x + 3*xscale, y - 14*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 3*xscale, y - 12*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 1*xscale, y - 11*yscale, 1*xscale, 1*yscale);
+			context.fillRect(x + 6*xscale, y - 11*yscale, 4*xscale, 1*yscale);
+			context.fillRect(x + 7*xscale, y - 12*yscale, 1*xscale, 1*yscale);
+			context.fillStyle = "#000000"
+			context.fillRect(x + 6*xscale, y - 14*yscale, 1*xscale, 2*yscale);
+		}
 
 		if (this.wideEyed) {
-			context.fillRect(x + 38, y - 34, 8, 2);
+			// context.fillRect(x + 38, y - 34, 8, 2);
 		} else {
-			context.fillRect(x + 40, y - 36, 8, 2);
+			// context.fillRect(x + 40, y - 36, 8, 2);
 		}
 
-		// arm (singular)
-		context.fillRect(x + 36, y - 26, 4, 2);
-		context.fillRect(x + 40, y - 26, 2, 4);
 
 		y = offsetY;
 		if (this.hasBackLegUp(offset)) {
 			y -= 4;
 		}
 		// back leg
-		context.fillRect(x + 12, y, 4, 2);
-		context.fillRect(x + 12, y - 6, 2, 8);
-		context.fillRect(x + 14, y - 6, 2, 3);
-		context.fillRect(x + 16, y - 8, 2, 3);
+		context.fillStyle = "#00000f"
+		context.fillRect(x + 5*xscale, y - 3*yscale, 3*xscale, 2*yscale);
+		context.fillRect(x + 8*xscale, y - 2*yscale, 1*xscale, 1*yscale);
+
 
 		y = offsetY;
 		if (this.hasFrontLegUp(offset)) {
@@ -128,8 +171,8 @@
 		}
 
 		// front leg
-		context.fillRect(x + 22, y, 4, 2);
-		context.fillRect(x + 22, y - 6, 2, 8);
+		context.fillRect(x + 2*xscale, y - 2*yscale, 3*xscale, 2*yscale);
+		context.fillRect(x + 5*xscale, y - 1*yscale, 1*xscale, 1*yscale);
 	};
 
 	Dinosaur.prototype.colliders = function(offset) {
